b317131ce809a118a5839b674e22749bacdd6ed5
sylocse <90429687+sylocse@users.noreply.github.com>
2021-11-15 15:48:36 -0800

    update index.html and main.js

--- FrontEndTable/main.js
+++ FrontEndTable/main.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(function() {
     var table = $('#table').DataTable();
 
@@ -8,3 +9,48 @@ $(document).ready(function() {
         alert( table.rows('.selected').data().length +' row(s) selected to Hide' );
     } );
 } );
+*/
+
+
+
+$(document).ready(function () {
+    $("#example").DataTable();
+
+    /* Add a click handler to the rows - this could be used as a callback */
+    $("#example tbody tr").click(function (e) {
+        if ($(this).hasClass('row_selected')) {
+            $(this).removeClass('row_selected');
+        }
+        else {
+            oTable.$('tr.row_selected')// multiple selected  //.removeClass('row_selected'); 
+            $(this).addClass('row_selected');
+        }
+    });
+
+    /* Add a click handler for the delete row */
+    $('#hide').click(function () {
+        var anSelected = fnGetSelected(oTable);
+        $(anSelected).hide();
+    });
+
+    $('#show').click(function () {
+        var anSelected = fnGetSelected(oTable);
+        $(anSelected).show();
+    });
+
+    $('#newsearch').click(function () {
+        $('table.display').DataTable();
+    });
+
+    /* Init the table */
+    oTable = $('#example').dataTable();
+
+    /* Get the rows which are currently selected */
+    function fnGetSelected(oTableLocal) {
+        return oTableLocal.$('tr.row_selected');
+    }
+
+
+});
+
+var oTable;
